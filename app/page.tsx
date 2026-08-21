299d274ce787eedac078f0cadfcc72288dd23b5d
طه اليافعي <Tahaaljabre@hotmail.com>
2026-08-21 05:39:39 +0300

    Make school login easy to find

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,12 @@
 import SchoolApp from "./SchoolApp";
-export default function Home(){return <SchoolApp/>}
+import { redirect } from "next/navigation";
+import { getChatGPTUser } from "./chatgpt-auth";
+import { externalIdentity } from "../db/external-auth";
+
+export const dynamic = "force-dynamic";
+
+export default async function Home(){
+ const identity=(await externalIdentity())??(await getChatGPTUser());
+ if(!identity)redirect("/login");
+ return <SchoolApp/>;
+}
